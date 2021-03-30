b53382734d638f18440199f7b765e3faa436a7ec
Leo Zhang <leotheasianlion@gmail.com>
2021-03-30 02:05:35 -0400

    working on websockets

--- student_connection_platform_frontend/lib/pages_by_leo/dm_chat.dart
+++ student_connection_platform_frontend/lib/pages_by_leo/dm_chat.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 import '../constants.dart';
 
-class DMChat extends StatefulWidget {
+class DMChat extends StatefulWidget
+{
+  final WebSocketChannel channel;
+
+  const DMChat({Key key, this.channel}) : super(key: key);
+
   @override
   _DMChatState createState() => _DMChatState();
 }
 
-class _DMChatState extends State<DMChat> {
+class _DMChatState extends State<DMChat>
+{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
